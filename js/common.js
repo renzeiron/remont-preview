f3ca5ee5b3bf6d626942da0351f174c1950c5703
Andrew Stoliar <poisonconsumed@gmail.com>
2017-10-17 20:32:18 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -7,4 +7,34 @@ $(document).ready(function() {
 			$('#navbarLogo').fadeOut();
 		}
 	});
+
+	$('#navigation .navbar-nav li>a').click(function() {
+		var link = $(this).attr('href');
+		var pos = $(link).offset().top;
+		$('body,html').animate({ scrollTop: pos }, 700);
+	});
+
+	$('#navigation .navbar-brand').click(function() {
+		var link = $(this).attr('href');
+		var pos = $(link).offset().top;
+		$('body,html').animate({ scrollTop: pos }, 700);
+	});
+
+    sections = $('.section'),
+    nav = $('.navbar-nav');
+	$(window).on('scroll', function () {
+    	var pos = $(this).scrollTop();
+		
+      	sections.each(function () {
+        	var top = $(this).offset().top,
+			bottom = top + $(this).outerHeight();
+			
+			if (pos >= top && pos <= bottom) {
+				nav.children('.nav-item').find('a').removeClass('active');
+				nav.children('.nav-item').find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+			} else {
+				nav.children('.nav-item').find('a[href="#' + $(this).attr('id') + '"]').removeClass('active');
+			}
+      	});
+	});
 });
