29c3b443367d2690e1c4010936395705ea0707bf
Andrew Stoliar <poisonconsumed@gmail.com>
2017-10-17 12:54:56 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,5 +1,10 @@
-$(function() {
-
-	// Custom JS
-
+$(document).ready(function() {
+	$(document).scroll(function () {
+		var y = $(this).scrollTop();
+		if (y > 300) {
+			$('#navbarLogo').fadeIn();
+		} else {
+			$('#navbarLogo').fadeOut();
+		}
+	});
 });
