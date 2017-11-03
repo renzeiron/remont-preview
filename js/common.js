8d6f18e6c4098ffe96262cce7c27559e3337aa0c
Andrew Stoliar <poisonconsumed@gmail.com>
2017-11-03 20:07:50 +0200

    new features implemented

--- js/common.js
+++ js/common.js
@@ -1,14 +1,6 @@
 $(document).ready(function() {
-	// $(document).scroll(function () {
-	// 	var y = $(this).scrollTop();
-	// 	if (y > 300) {
-	// 		$('#navbarLogo').fadeIn();
-	// 	} else {
-	// 		$('#navbarLogo').fadeOut();
-	// 	}
-	// });
 
-	$('#navigation .navbar-nav li>a').click(function(e) {
+	$('#navigation .navbar-nav li > a').click(function(e) {
 		e.preventDefault(); 
 		var link = $(this).attr('href');
 		var pos = $(link).offset().top;
@@ -22,8 +14,50 @@ $(document).ready(function() {
 		$('body,html').animate({ scrollTop: pos }, 700);
 	});
 
+	$('a').click(function(e) {
+		$(this).blur();
+		e.preventDefault();
+	});
+
+	$('#calculate').click(function(){
+		var total = 0;
+
+		$('#calc-body tbody tr').each(function() {
+			var price;
+			
+			$(this).find('.td-price').each(function() {
+				price = parseInt($(this).html());
+			});
+
+			$(this).find('input').each(function(index, elem) {
+				var val = parseInt($(elem).val());
+				if (!isNaN(val)) {
+					total += val * price;
+				}
+			});
+		});
+
+		$('#total').text('Итого: ' + total + ' грн.');
+	});
+
+	(function() {
+		'use strict';	  
+		window.addEventListener('load', function() {
+			var form = document.getElementById('contact-form');
+		  	form.addEventListener('submit', function(event) {
+				if (form.checkValidity() === false) {
+			  		event.preventDefault();
+			  		event.stopPropagation();
+				}
+
+				$('#needs-validation').addClass('was-validated');
+		  	}, false);
+		}, false);
+	})();
+
     sections = $('.section'),
-    nav = $('.navbar-nav');
+	nav = $('.navbar-nav');
+
 	$(window).on('scroll', function () {
     	var pos = $(this).scrollTop();
 		
@@ -45,4 +79,5 @@ $(document).ready(function() {
 			}
       	});
 	});
+	
 });
