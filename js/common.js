a85a351bb9c355579fd711835f283d26a562b72d
Andrew Stoliar <poisonconsumed@gmail.com>
2017-11-20 17:05:11 +0200

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -60,28 +60,13 @@ $(document).ready(function() {
 		$('#total').text('Итого: ' + total + ' грн.');
 	});
 
-	(function() {
-		'use strict';	  
-		window.addEventListener('load', function() {
-			var form = document.getElementById('contact-form');
-		  	form.addEventListener('submit', function(event) {
-				if (form.checkValidity() === false) {
-			  		event.preventDefault();
-			  		event.stopPropagation();
-				}
-
-				$('#needs-validation').addClass('was-validated');
-		  	}, false);
-		}, false);
-	})();
-
     sections = $('.section'),
 	nav = $('.navbar-nav');
 
 	$(window).on('scroll', function () {
     	var pos = $(this).scrollTop() + $('#navigation').outerHeight();
 		
-		if (pos >= /*300*/ $('#top').outerHeight()) {
+		if (pos >= $('#top').outerHeight()) {
 			$('#navbarLogo').fadeIn();
 		} else {
 			$('#navbarLogo').fadeOut();
@@ -109,3 +94,73 @@ $(document).ready(function() {
 	});
 
 });
+
+function addEvent(node, type, callback) {
+	if (node.addEventListener) {
+		node.addEventListener(
+			type,
+			function(e) {
+				  callback(e, e.target);
+			},
+			false
+		);
+	} else if (node.attachEvent) {
+		node.attachEvent("on" + type, function(e) {
+			callback(e, e.srcElement);
+		});
+	}
+}
+
+function shouldBeValidated(field) {
+	return (
+		!(field.getAttribute("readonly") || field.readonly) &&
+		  !(field.getAttribute("disabled") || field.disabled) &&
+		  (field.getAttribute("pattern") || field.getAttribute("required"))
+	);
+}
+
+function instantValidation(field) {
+	if (shouldBeValidated(field)) {
+		//the field is invalid if:
+		  //it's required but the value is empty
+		  //it has a pattern but the (non-empty) value doesn't pass
+		var invalid =
+			(field.getAttribute("required") && !field.value) ||
+			(field.getAttribute("pattern") &&
+			field.value &&
+			!new RegExp(field.getAttribute("pattern")).test(field.value));
+  
+		  //add or remove the attribute is indicated by
+		  //the invalid flag and the current attribute state
+		  if (!invalid && field.getAttribute("aria-invalid")) {
+			field.removeAttribute("aria-invalid");
+		  } else if (invalid && !field.getAttribute("aria-invalid")) {
+			field.setAttribute("aria-invalid", "true");
+		}
+	}
+}
+
+(function() {
+	'use strict';	  
+	window.addEventListener('load', function() {
+		var form = document.getElementById('contact-form');
+		var fields = [ form.getElementsByTagName("input") ];
+		
+		for (var a = fields.length, i = 0; i < a; i++) {
+			for (var b = fields[i].length, j = 0; j < b; j++) {
+				  addEvent(fields[i][j], "change", function(e, target) {
+					instantValidation(target);
+				});
+			}
+		}
+		
+		form.addEventListener('submit', function(event) {
+			if (form.checkValidity() === false) {
+				event.preventDefault();
+			  event.stopPropagation();
+			}
+
+			$('#needs-validation').addClass('was-validated');
+		}, false);
+	}, false);
+})();
